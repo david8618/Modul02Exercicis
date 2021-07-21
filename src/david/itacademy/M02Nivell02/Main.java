9ce40963241898ef4d9e2b30d1923266d0162819
david8618 <ddiaz.8618@gmail.com>
2021-07-21 06:44:32 +0200

    Exercicis Modul 02. Modificat l'exercici M02Fase02 i Nivell02

--- src/david/itacademy/M02Nivell02/Main.java
+++ src/david/itacademy/M02Nivell02/Main.java
@@ -1,5 +1,7 @@
 package david.itacademy.M02Nivell02;
 
+import java.util.Scanner;
+
 public class Main {
     /*
     Crea una aplicació que dibuixi una escala de nombres,
@@ -9,10 +11,14 @@ public class Main {
     Nosaltres li vam passar l'altura de la piràmide per teclat. Aquest és un exemple:
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         //Primera part
+        int e;
+        System.out.println("Digita els nombres que vols que tingui l'escala: ");
+        e=sc.nextInt();
         System.out.print("Escala de nombres:");
-        for (int i = 1; i <=8; i++) {
+        for (int i = 1; i <=e; i++) {
             System.out.println();
             for (int j = 1; j <=i ; j++) {
                 System.out.print(j+" ");
@@ -20,15 +26,22 @@ public class Main {
         }
         System.out.println("\n--------------------");
         //Segona part
-        System.out.println("Piràmide invertida:");
-        for (int i = 1; i <= 8; i++) {
+        int n;
+        //Demanen al usuari a alçada de la piràmide
+
+        System.out.println("¿Quina alçada vols que tingui la piràmide?");
+        n=sc.nextInt();
+        System.out.println("La Piràmide invertida amb una alçada de " + n +" :");
+        for (int i = 1; i <= n; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 8-1-i; j >= 0; j--) {
+            for (int j = n-1-i; j >= 0; j--) {
                 System.out.print("* ");
             }
             System.out.println();
         }
+
+        sc.close();
     }
 }
