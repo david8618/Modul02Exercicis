3786cb617513d7e025ff4574560a36a196d5b92d
david8618 <ddiaz.8618@gmail.com>
2021-07-21 06:35:20 +0200

    Exercicis Modul 02. Modificado el ejercicio M02Fase02.

--- src/david/itacademy/M02Fase02/Main.java
+++ src/david/itacademy/M02Fase02/Main.java
@@ -14,10 +14,9 @@ public class Main {
     public static void main(String[] args) {
         //Llista
         List<Character> nom = Arrays.asList('d','a','v','i','d');
-        //Array de vocals, consontants y numeros
+        //Array de vocals, consontants
         char[] vocals = {'a','e','i','o','u'};
         char[] consonants ={'b','c','d','f','g','h','j','k','l','m','n','ñ','p','q','r','s','t','v','w','x','y','z'};
-        char[] numeros={'0','1','2','3','4','5','6','7','8','9'};
 
         //Comprobem si dintra de la Llista hi han vocals,consonants o numeros.
         for (int i = 0; i < nom.size(); i++) {
@@ -31,11 +30,7 @@ public class Main {
                     System.out.println(nom.get(i) + " es una Consonant.");
                 }
             }
-            for (int n = 0; n < numeros.length; n++) {
-                if(nom.get(i)==numeros[n]) System.out.println("Els noms de persones no contenen números!");
-
-            }
-            
+            if(Character.isDigit(nom.get(i))==true) System.out.println("Els noms de personas no contenen números!");
         }
 
     }
